b27acf5a28146ceaea48cab2c8f02a13e206fe0c
devhalo21028 <jayson.unto.ojeda@gmail.com>
2022-06-01 21:58:17 +0800

    feat: NIH-11 CICD Pipeline Role Infrastructure (#1)
    #### References
    - https://devhalos.atlassian.net/browse/NIH-11
    
    #### Changes
    - docs: update license to mit
    - chore: setup project scripts
    - chore(cicd-pipeline-role): add terraform config for creating role that github can assume
    
    #### Samples
    ![2022-06-01_21-43](https://user-images.githubusercontent.com/5345015/171420552-9b7732f9-6900-4994-95b3-070e3dea8b9f.png)

---
+++ cicd-pipeline-role/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_account" {
+  type = string
+}
+
+variable "organization" {
+  type    = string
+  default = "devhalos"
+}
+
+variable "project" {
+  type    = string
+  default = "nihil"
+}
+
+variable "component" {
+  type    = string
+  default = "cicd-pipeline"
+}

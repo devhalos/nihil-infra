a726db2a7c259247f6408a1d38c3391cf7c23b83
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-06-15 19:20:09 +0800

    chore: update remote backend module reference

--- nihil-state/main.tf
+++ nihil-state/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "jenkins_casc" {
-  source    = "git@github.com:devhalos/nihil-terraform-modules.git//aws_s3_remote_backend?ref=v0.1.0"
+  source    = "github.com/devhalos/nihil-terraform-modules.git//aws_s3_remote_backend?ref=v0.1.0"
   component = "jenkins-casc"
 }
 

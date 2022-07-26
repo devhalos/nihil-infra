aefcf6cebb46d23705cdb4281a527a72eb12edb3
devhalo21028 <jayson.unto.ojeda@gmail.com>
2022-07-26 22:17:04 +0800

    feat: NIH-12 Simplify Component Terraform Remote Backend Process (#3)
    * chore(nihil-state): add config for nihil state
    
    * ci(nihil-state): add pipeline for deploying nihil state infra
    
    * ci(cicd-pipeline-role): update cicd pipeline role permissions
    
    * ci(nihil-infra): run deployment only on main and tags

---
+++ nihil-state/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "devhalos-nihil-state"
+    key    = "remote-backend/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+

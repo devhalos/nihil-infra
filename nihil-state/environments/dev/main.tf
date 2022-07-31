cdcd8ea20277cd0117808ba5a484a0876fee62bf
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-07-31 22:25:51 +0800

    feat: add deployment env development

--- nihil-state/environments/dev/main.tf
+++ nihil-state/environments/dev/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 module "jenkins_casc" {
   source    = "github.com/devhalos/nihil-terraform-modules.git//aws_s3_remote_backend?ref=v0.1.0"
-  component = "jenkins-casc"
+  component = "jenkins-casc-${terraform.workspace}"
 }
 

6603dc9ea39333d9556d547972192ca059e6cb7c
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-08-30 22:45:32 +0800

    chore(remote-backend): remove workspace in component name

--- remote-backend/main.tf
+++ remote-backend/main.tf
@@ -11,12 +11,12 @@ provider "aws" {
 
 module "jenkins_casc" {
   source    = "github.com/devhalos/nihil-terraform-modules.git//aws_s3_remote_backend?ref=v0.2.0"
-  component = "${terraform.workspace}-jenkins-casc"
+  component = "jenkins-casc"
 }
 
 module "jenkins_casc_docker_registry" {
   source    = "github.com/devhalos/nihil-terraform-modules.git//aws_s3_remote_backend?ref=v0.2.0"
-  component = "${terraform.workspace}-jenkins-casc-docker-registry"
+  component = "jenkins-casc-docker-registry"
 }
 
 

7240061e09106cc8a92f9b9123dd5122520830aa
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2023-10-04 20:15:04 +0800

    ci: add infra lint job

--- scripts/track-main-branch.sh
+++ scripts/track-main-branch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 current_branch=$(git branch --show-current)
+is_main_branch_exists=$(git show-ref --quiet 'refs/heads/main' && echo true || echo false)
 
-if [ "$current_branch" != 'main' ]; then
+if [ "$current_branch" != 'main' ] && [ "$is_main_branch_exists" != true ]; then
     git fetch origin main
     git branch --track main origin/main
 fi

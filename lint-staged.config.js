2a132fe7b0f41ed2c1133fe4c9f17baffd0e8fae
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2023-10-05 17:35:47 +0800

    ci: remove infra init on lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ module.exports = {
     `nx affected -t lint:type --files=${files.join(',')}`,
   'packages/**/*.{js,ts,jsx,tsx,json}': (files) =>
     `nx affected:lint --fix --files=${files.join(',')}`,
-  'packages/**/*.{tf}': (files) => files.map((file) => `terraform fmt ${file}`),
+  'packages/**/*.tf': (files) => files.map((file) => `terraform fmt ${file}`),
   '**/*.{js,ts,jsx,tsx,json,cjs,html,css}': (files) =>
     `nx format:write --files=${files.join(',')}`,
 };
